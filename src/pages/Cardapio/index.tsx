9ee8a0b897daa6c4b656798defd0650667233b59
LenaMoromisato <moromisato.lena@gmail.com>
2023-07-07 17:22:25 -0300

    Tentando importar o svg

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -3,8 +3,8 @@ import styles from './Cardapio.module.scss';
 export default function Cardapio() {
     return (
         <main>
-            <nav className="menu">
-
+            <nav className={styles.menu}>
+                <img src="../../assets/logo.svg" alt="logo do aluroni" />
             </nav>
         </main>
     )
