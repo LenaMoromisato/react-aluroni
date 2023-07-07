6b347c78c718c2628f3a4bce12dd8dd83b21962b
LenaMoromisato <moromisato.lena@gmail.com>
2023-07-07 17:46:01 -0300

    Importando a logo

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,10 +1,15 @@
 import styles from './Cardapio.module.scss';
+// import logo from '../../assets/logo.svg'; -- importar para o reconhecimento do arquivo estático
+import {ReactComponent as Logo} from '../../assets/logo.svg'; // converte svg em componete react
 
 export default function Cardapio() {
     return (
         <main>
             <nav className={styles.menu}>
-                <img src="../../assets/logo.svg" alt="logo do aluroni" />
+                {
+                    // <img src={logo} alt="logo do aluroni" />
+                }
+                <Logo />
             </nav>
         </main>
     )
