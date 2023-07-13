26922b47bc209d31c520ebd76980c027707b33bb
LenaMoromisato <moromisato.lena@gmail.com>
2023-07-13 17:36:25 -0300

    fazendo o ordenador funcionar

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -8,6 +8,7 @@ import Ordenador from './Ordenador';
 export default function Cardapio() {
     const [busca, setBusca] = useState("");
     const [filtro, setFiltro] = useState<number | null>(null);
+    const [ordenador, setOrdenador] = useState("");
     return (
         <main>
             <nav className={styles.menu}>
@@ -23,7 +24,7 @@ export default function Cardapio() {
                 <Buscador busca={busca} setBusca={setBusca} />
                 <div className={styles.cardapio__filtros}>
                     <Filtros filtro={filtro} setFiltro={setFiltro} />
-                    <Ordenador />
+                    <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
                 </div>
             </section>
         </main>
