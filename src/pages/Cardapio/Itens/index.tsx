4612be77ae7c3768cd45eb3dd05e4e9641c33b38
LenaMoromisato <moromisato.lena@gmail.com>
2023-07-14 17:07:31 -0300

    buscador e filtros

--- src/pages/Cardapio/Itens/index.tsx
+++ src/pages/Cardapio/Itens/index.tsx
@@ -1,11 +1,37 @@
 import cardapio from './itens.json';
 import Item from './Item';
 import styles from './Itens.module.scss';
+import { useState, useEffect } from "react";
 
-export default function Itens() {
+interface Props {
+    busca: string;
+    filtro: number | null;
+    ordenador: string;
+}
+
+export default function Itens(props: Props) {
+    const [lista, setLista] = useState(cardapio);
+    const { busca, filtro } = props;
+
+    function testaBusca(title: string) {
+        const regex = new RegExp(busca, 'i'); // Essa busca vai ser case sensitive, então se colocar “macarrão”, ele não vai entender, porque o macarrão que está no título, está maiúsculo, “Macarrão”. Então para resolvermos esse problema, colocamos a vírgula dentro do RegExp, e o segundo parâmetro dele vai ser um i, que é uma string i. Nós dizemos para o RegExp que ele vai ser case insensitive, isso significa que se colocarmos o m, não vai importar, porque ele sempre vai testar só pela string, não por ser minúsculo ou maiúsculo, que é o que queremos.
+
+        return regex.test(title);
+    }
+
+    function testaFiltro(id: number) {
+        if(filtro !== null) return filtro === id;
+        return true;
+    }
+
+    useEffect(() => {
+        const novaLista = cardapio.filter(item => testaBusca(item.title) && testaFiltro(item.category.id));
+        setLista(novaLista);
+    },[busca, filtro]);
+
     return (
         <div className={styles.itens}>
-            {cardapio.map(item => (
+            {lista.map(item => (
                 <Item key={item.id} {...item} />
             ))}
         </div>
