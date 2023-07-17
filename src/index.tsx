6a0a4eafba740270c67b0628cd60c3c3c3697e46
LenaMoromisato <moromisato.lena@gmail.com>
2023-07-17 16:15:41 -0300

    pratos recomendados

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './index.css';
 import Cardapio from './pages/Cardapio';
+import Inicio from 'pages/Inicio';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Cardapio />
+    <Inicio />
   </React.StrictMode>,
   document.getElementById('root')
 );
