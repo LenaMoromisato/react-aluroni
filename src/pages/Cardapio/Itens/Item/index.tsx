21d5a4cdab615840dfe95d63e7585a1ff4652738
LenaMoromisato <moromisato.lena@gmail.com>
2023-07-18 18:52:47 -0300

    NotFound na tela de detalhes

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,11 +1,13 @@
 import { Prato } from 'types/Prato';
 import styles from './Item.module.scss'; 
 import TagsPrato from 'components/TagsPrato';
+import { useNavigate } from 'react-router-dom';
 
 export default function Item(props: Prato) {
-  const { title, description, photo } = props;
+  const { id, title, description, photo } = props;
+  const navigate = useNavigate();
   return (
-    <div className={styles.item}>
+    <div className={styles.item} onClick={() => navigate(`/prato/${id}`)}>
       <div className={styles.item__imagem}>
         <img src={photo} alt={title} />
       </div>
--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -2,13 +2,14 @@ import styles from './Prato.module.scss';
 import { useParams, useNavigate } from 'react-router-dom';
 import cardapio from 'data/cardapio.json';
 import TagsPrato from 'components/TagsPrato';
+import NotFound from 'pages/NotFound';
 
 export default function Prato() {
   const { id } = useParams();
   const navigate = useNavigate();
   const prato = cardapio.find(item => item.id === Number(id));
   if(!prato) {
-    return '';
+    return <NotFound />;
   }
   return (
     <>
